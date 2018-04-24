0d42234a1c855977372b1749792fdfe24130cc64
Viktoras Makauskas <viktoras@neglostyti.com>
2018-04-24 07:49:36 +0300

    brainstorming

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,44 @@
 #include <vector>
 #include <functional>
+#include <iostream>
 
 #include "candidate_points.h"
 #include "image_sources.h"
 
+
+#define LOG(msg) std::cout << msg << std::endl;
+
+inline double distance(const cv::Point2d &src, const cv::Point2d &dest) {
+	// square distance will be fine probably
+	auto dx = src.x - dest.x;
+	auto dy = src.y - dest.y;
+	return dx * dx + dy * dy;
+}
+
+inline void calcGrid(const std::vector<cv::Point2d> &points) {
+	std::vector<double> distances;
+	distances.resize(points.size()*points.size());
+	LOG("reserved distances: " << distances.size());
+
+
+	const size_t numPoints = points.size();
+	for (size_t source = 0, const last = numPoints-1; source != last; source++) {
+		for (size_t dest = source + 1, lastDest = numPoints; dest != lastDest; dest++) {
+			const double d = distance(points.at(source), points.at(dest));
+			distances.at(source*numPoints + dest) = d;
+			distances.at(dest*numPoints + source) = d;
+		}
+
+	}
+	LOG("distances calculated");
+
+
+}
+
 inline void processImage(cv::Mat &source) {
 	std::vector<cv::Point2d> points;
 	findCandidatePoints(source, points);
+	calcGrid(points);
 
 
 	// preview intermediate result
